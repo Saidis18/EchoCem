4ea3c9e2c303eac55d39653c02ff24c46fd615f0
Said Tellez <tellezsaid@gmail.com>
2025-12-31 02:36:08 +0100

    Improved automation

--- enable_training.sh
+++ enable_training.sh
@@ -12,7 +12,8 @@ rsync -a --include='*.py' --exclude='*' "$SRC_DIR"/ "$OUT_DIR"/
 
 find "$OUT_DIR" -name "*.py" -print0 | xargs -0 -n1 strip-hints --inplace
 find "$OUT_DIR" -name "*.py" -print0 | xargs -0 autoflake --in-place --remove-all-unused-imports --remove-unused-variables
-find "$OUT_DIR" -name "*.py" -print0 | xargs -0 black
+find "$OUT_DIR" -name "*.py" -print0 | xargs -0 sed -i -E '/^\s*_\w+_t = .+/d'
+find "$OUT_DIR" -name "*.py" -print0 | xargs -0 black --line-length 999 
 
 source .secrets
 scp -r -P $CONT_PORT "$OUT_DIR"/* $CONT_USER@$CONT_HOST:$CONT_PATH
