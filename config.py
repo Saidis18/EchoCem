e73bcebbf58442d45f08d625046f6aa91ed184ca
Said Tellez <tellezsaid@gmail.com>
2026-01-01 22:56:56 +0100

    Refactor to allow for shared and centralized config

---
+++ config.py
@@ -0,0 +1,53 @@
+from u_net import DiceCELoss
+import data
+import torch
+import torch.utils.data # type: ignore
+from torchvision import transforms # type: ignore
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    loss_fn: torch.nn.Module
+    features: list[int]
+    transform: transforms.Compose | None
+    epochs: int
+    batch_size_train: int
+    batch_size_val: int
+
+    DATA_DIR: data.Path = data.Path(__file__).parent / "data"
+    X_TRAIN_DIR: data.Path = DATA_DIR / "X_train_uDRk9z9" / "images"
+    Y_TRAIN_CSV: data.Path = DATA_DIR / 'Y_train_T9NrBYo.csv'
+    X_TEST_DIR: data.Path = DATA_DIR / "X_test_xNbnvIa" / "images"
+    RUNS_DIR: data.Path = data.Path(__file__).parent / "runs"
+
+
+std_configs = [
+    Config(
+        loss_fn=torch.nn.CrossEntropyLoss(),
+        features=[64, 128, 256, 512],
+        transform=transforms.Compose([
+            transforms.Lambda(lambda img: img.crop((0, 0, min(img.width, 160), img.height))), # type: ignore
+            transforms.ToTensor()
+        ]),
+        epochs=30,
+        batch_size_train=128,
+        batch_size_val=64
+    ),
+    Config(
+        loss_fn=torch.nn.CrossEntropyLoss(),
+        features=[64, 128, 256],
+        transform=transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()]),
+        epochs=30,
+        batch_size_train=128,
+        batch_size_val=64
+    ),
+    Config(
+        loss_fn=DiceCELoss(),
+        features=[64, 128, 256],
+        transform=transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()]),
+        epochs=30,
+        batch_size_train=128,
+        batch_size_val=64
+    )
+]
--- training.py
+++ training.py
@@ -1,54 +1,27 @@
-from u_net import Segmentation, UNet, DiceCELoss
+from u_net import Segmentation, UNet
 import data
 import torch
 import torch.utils.data # type: ignore
-from torchvision import transforms # type: ignore
 from torch.utils.data import DataLoader
+import config
 
 
 RUN_NUM = 3
 TESTING = True
 
-if RUN_NUM == 1: # type: ignore
-    loss = torch.nn.CrossEntropyLoss()
-    features = [64, 128, 256, 512]
-    transform = transforms.Compose([
-        transforms.Lambda(lambda img: img.crop((0, 0, min(img.width, 160), img.height))), # type: ignore
-        transforms.ToTensor()
-    ])
-    epochs = 30
-    batch_size_train = 128
-    batch_size_val = 64
-elif RUN_NUM == 2: # type: ignore
-    loss = torch.nn.CrossEntropyLoss()
-    features = [64, 128, 256]
-    transform = transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()])
-    epochs = 30
-    batch_size_train = 128
-    batch_size_val = 64
-elif RUN_NUM == 3: # type: ignore
-    loss = DiceCELoss()
-    features = [64, 128, 256]
-    transform = transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()])
-    epochs = 30
-    batch_size_train = 128
-    batch_size_val = 64
-else:
+try:
+    conf = config.std_configs[RUN_NUM-1]
+except IndexError:
     raise ValueError(f"Invalid RUN_NUM: {RUN_NUM}")
 
-
 if TESTING:
-    epochs = 2
-    batch_size_train = 4
-    batch_size_val = 4
+    conf.epochs = 2
+    conf.batch_size_train = 4
+    conf.batch_size_val = 4
+
 
 if __name__ == "__main__":
-    # Dataset
-    DATA_DIR = data.Path(__file__).parent / "data"
-    X_DIR = DATA_DIR / "X_train_uDRk9z9" / "images"
-    Y_CSV = DATA_DIR / 'Y_train_T9NrBYo.csv'
-
-    dataset = data.EchoCementDataset(X_DIR, Y_CSV, transform=transform)
+    dataset = data.EchoCementDataset(conf.X_TRAIN_DIR, conf.Y_TRAIN_CSV, transform=conf.transform)
     if TESTING:
         dataset = torch.utils.data.Subset(dataset, list(range(64)))
     
@@ -56,16 +29,17 @@ if __name__ == "__main__":
     print(f"Train dataset size: {len(train_dataset)}, Validation dataset size: {len(val_dataset)}")
     
     # Dataloaders
-    train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, num_workers=6)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size_val, shuffle=False, num_workers=6)
+    train_loader = DataLoader(train_dataset, batch_size=conf.batch_size_train, shuffle=True, num_workers=6)
+    val_loader = DataLoader(val_dataset, batch_size=conf.batch_size_val, shuffle=False, num_workers=6)
     
     # Model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    base_model = UNet(in_channels=1, out_channels=3, features=features)
-    model = Segmentation(base_model=base_model, loss_fn=loss).to(device)
+    base_model = UNet(in_channels=1, out_channels=3, features=conf.features)
+    model = Segmentation(base_model=base_model, loss_fn=conf.loss_fn).to(device)
     print(f"Trainable parameters: {model.param_count}")
     print(f"Using device: {device}")
 
-    model.training_loop(train_loader, val_loader, epochs=epochs, device=device)
+    model.training_loop(train_loader, val_loader, epochs=conf.epochs, device=device)
 
-    torch.save(model.state_dict(), f"runs/unet_model_{RUN_NUM}.pt")
+    torch.save(model.state_dict(), conf.RUNS_DIR / f"unet_model_{RUN_NUM}.pt")
+    print(f"Model saved to {conf.RUNS_DIR / f'unet_model_{RUN_NUM}.pt'}")
