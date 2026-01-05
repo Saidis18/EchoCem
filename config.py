13de60f309192c85d6c056d4c4a5d804df851f1c
Said Tellez <tellezsaid@gmail.com>
2026-01-05 22:11:01 +0100

    config no longer needs transformations

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ import torch
 from torchvision import transforms # type: ignore
 
 
-Transformation = transforms.Compose | torch.nn.Module
-
 class Config():
     DATA_DIR = Path(__file__).parent / "data"
     RUNS_DIR = Path(__file__).parent / "runs"
@@ -16,16 +14,12 @@ class Config():
             self,
             loss_fn: torch.nn.Module,
             features: list[int],
-            trans_in: Transformation,
-            trans_out: Transformation,
             epochs: int,
             batch_size_train: int,
             batch_size_val: int
         ):
         self.loss_fn = loss_fn
         self.features = features
-        self.trans_in = trans_in
-        self.trans_out = trans_out
         self.epochs = epochs
         self.batch_size_train = batch_size_train
         self.batch_size_val = batch_size_val
@@ -34,11 +28,6 @@ std_configs = [
     Config(
         loss_fn=torch.nn.CrossEntropyLoss(),
         features=[64, 128, 256],
-        trans_in=transforms.Compose([
-            transforms.Lambda(lambda img: img.crop((0, 0, min(img.width, 160), img.height))), # type: ignore
-            transforms.ToTensor()
-        ]),
-        trans_out=transforms.Pad((0, 0, 272 - 160, 0)),
         epochs=30,
         batch_size_train=64,
         batch_size_val=128
@@ -46,11 +35,6 @@ std_configs = [
     Config(
         loss_fn=TVCELoss(),
         features=[64, 128, 256],
-        trans_in=transforms.Compose([
-            transforms.Lambda(lambda img: img.crop((0, 0, min(img.width, 160), img.height))), # type: ignore
-            transforms.ToTensor()
-        ]),
-        trans_out=transforms.Pad((0, 0, 272 - 160, 0)),
         epochs=15,
         batch_size_train=16,
         batch_size_val=128
@@ -58,8 +42,6 @@ std_configs = [
     Config(
         loss_fn=torch.nn.CrossEntropyLoss(),
         features=[64, 128, 256],
-        trans_in=transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()]),
-        trans_out=transforms.Resize((160, 272), interpolation=transforms.InterpolationMode.NEAREST),
         epochs=30,
         batch_size_train=128,
         batch_size_val=64
@@ -67,8 +49,6 @@ std_configs = [
     Config(
         loss_fn=DiceCELoss(),
         features=[64, 128, 256],
-        trans_in=transforms.Compose([transforms.Resize((160, 160)), transforms.ToTensor()]),
-        trans_out=transforms.Resize((160, 272), interpolation=transforms.InterpolationMode.NEAREST),
         epochs=30,
         batch_size_train=128,
         batch_size_val=64
