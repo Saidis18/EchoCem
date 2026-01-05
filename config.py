ae848130e37d90cbc2ef6f142c9ec8e51e1fec8d
Said Tellez <tellezsaid@gmail.com>
2026-01-05 06:27:26 +0100

    Revert "Added config for new test"
    This reverts commit fad314b6b78e303de2095b698696339758f5e2aa.

--- config.py
+++ config.py
@@ -33,18 +33,6 @@ class Config():
 
 std_configs = [
     Config(
-        loss_fn=torch.nn.CrossEntropyLoss(),
-        features=[64, 128, 256],
-        trans_in=transforms.Compose([
-            transforms.Lambda(lambda img: img.crop((0, 0, min(img.width, 160), img.height))), # type: ignore
-            transforms.ToTensor()
-        ]),
-        trans_out=transforms.Pad((0, 0, 272 - 160, 0)),
-        epochs=30,
-        batch_size_train=64,
-        batch_size_val=128
-    ),
-    Config(
         loss_fn=TVCELoss(),
         features=[64, 128, 256],
         trans_in=transforms.Compose([
@@ -52,7 +40,7 @@ std_configs = [
             transforms.ToTensor()
         ]),
         trans_out=transforms.Pad((0, 0, 272 - 160, 0)),
-        epochs=15,
+        epochs=30,
         batch_size_train=16,
         batch_size_val=128
     ),
