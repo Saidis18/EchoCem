726c98cf0d723623800ff05b6db8f2073a79fa7c
Said Tellez <tellezsaid@gmail.com>
2026-01-13 21:08:47 +0100

    Implemented self supervised pre training

--- training.py
+++ training.py
@@ -3,7 +3,6 @@ import data
 import torch
 import torch.utils.data # type: ignore
 import config
-import copy
 
 
 RUN_NUM = 2
@@ -22,33 +21,36 @@ if TESTING:
 
 
 if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if PRE_TRAIN:
         print(f"Starting pre-training...")
-        pretrain_conf = copy.deepcopy(conf)
-        pretrain_conf.loss_fn = torch.nn.MSELoss()
-        dataset = data.PreTrainingDataset(pretrain_conf.PRETRAINING_PATHS)
-        data_handler = data.DataHandler(dataset, conf=pretrain_conf, testing=TESTING)
+        dataset = data.PreTrainingDataset(conf.PRETRAINING_PATHS)
+        data_handler = data.DataHandler(dataset, conf=conf, testing=TESTING)
         
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        base_model = UNet(in_channels=1, out_channels=1, features=pretrain_conf.features)
-        model = Segmentation(base_model=base_model, conf=pretrain_conf).to(device)
+        base_model = UNet(in_channels=1, out_channels=1, features=conf.features)
+        model = Segmentation(base_model=base_model, conf=conf).to(device)
+        model.set_loss_fn(torch.nn.L1Loss())
         print(f"Trainable parameters: {model.param_count}")
-        print(f"Loss function: {pretrain_conf.loss_fn}")
+        print(f"Loss function: {model.loss_fn}")
         print(f"Using device: {device}")
 
         train_loader, val_loader = data_handler.get_loaders()
-        model.training_loop(train_loader, val_loader, epochs=pretrain_conf.epochs, device=device)
-        torch.save(model.state_dict(), pretrain_conf.RUNS_DIR / f"unet_model_{RUN_NUM}.pt")
-        print(f"Model saved to {pretrain_conf.RUNS_DIR / f'unet_model_{RUN_NUM}.pt'}")
+        model.training_loop(train_loader, val_loader, epochs=conf.epochs, device=device)
+
+        # Replace final_conv layer for 3-class segmentation task and move to device
+        model.base_model.final_conv = torch.nn.Conv2d(conf.features[0], 3, kernel_size=1).to(device)
+        model.reset_loss_fn()
+        print(f"Pre-training completed. Proceeding to segmentation training...")
+    else:
+        base_model = UNet(in_channels=1, out_channels=3, features=conf.features)
+        model = Segmentation(base_model=base_model, conf=conf).to(device)
     
     dataset = data.EchoCementDataset(conf.DATA_DIR / "X_train_uDRk9z9" / "images", conf.DATA_DIR / "Y_train_T9NrBYo.csv")
     data_handler = data.DataHandler(dataset, conf=conf, testing=TESTING)
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    base_model = UNet(in_channels=1, out_channels=3, features=conf.features)
-    model = Segmentation(base_model=base_model, conf=conf).to(device)
+
     print(f"Trainable parameters: {model.param_count}")
-    print(f"Loss function: {conf.loss_fn}")
+    print(f"Loss function: {model.loss_fn}")
     print(f"Using device: {device}")
 
     train_loader, val_loader = data_handler.get_loaders()
