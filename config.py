38842e83403e67c8ed5b4c52ff4f687a68122247
Said Tellez <tellezsaid@gmail.com>
2026-01-13 21:22:39 +0100

    Modified hyperparameters

--- config.py
+++ config.py
@@ -33,14 +33,14 @@ std_configs = [
     Config(
         loss_fn=torch.nn.CrossEntropyLoss(ignore_index=-100),
         features=[64, 128, 256, 512],
-        epochs=30,
+        epochs=20,
         batch_size_train=64,
         batch_size_val=128
     ),
     Config(
         loss_fn=torch.nn.CrossEntropyLoss(torch.tensor([0.6, 1.4, 1.2]), ignore_index=-100),
         features=[64, 128, 256, 512],
-        epochs=30,
+        epochs=20,
         batch_size_train=64,
         batch_size_val=128
     ),
